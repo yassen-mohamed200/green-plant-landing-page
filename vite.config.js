af1b25c7214e32b794f61617380b3035815a2594
yassen-mohamed200 <ym123yw@gmail.com>
2025-10-31 21:26:09 +0200

    Configure Vite for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/green-plant-landing-page/', // 👈 مهم جدًا علشان GitHub Pages يشتغل
   plugins: [
     tailwindcss(),
   ],
 })
+
